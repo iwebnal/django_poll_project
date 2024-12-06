7db76c672c0c82fe5d3773a175a8f88f44bb123d
iwebnal <iwebnal@gmail.com>
2024-12-06 12:56:38 +0300

    Viewses

--- polls/views.py
+++ polls/views.py
@@ -5,43 +5,26 @@ from django.template import loader
 from django.http import Http404
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    # latest_question_list = Question.objects.order_by("-pub_date")
-    # output = ", ".join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-    # latest_question_list = Question.objects.order_by("-pub_date")
-    # template = loader.get_template("polls/index.html")
-    # context = {
-    #     "latest_question_list": latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-
-    latest_question_list = Question.objects.order_by("-pub_date")
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    def get_queryset(self):
+        """Return published questions."""
+        return Question.objects.order_by("-pub_date")
 
 
-def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
-
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, "polls/detail.html", {"question": question})
-
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    # response = "You're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
